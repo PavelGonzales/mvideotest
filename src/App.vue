ff5504397ef187556071b5ce2c74f604d0e729d7
Pavel Gonzales <develop.gonzales@gmail.com>
2017-06-21 22:21:36 +0300

    добавил текст победителя и немного застилил, сделал вывод счета

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,29 @@
 <template>
   <div id="app">
     <img class="app_logo" src="./assets/logo.png">
-    <hello @start="next"></hello>
+    <hello v-if="!started" 
+           @start="next"></hello>
     <question v-for="question in questions" 
               :answerOne=question.answerOne
               :answerTwo=question.answerTwo
               :true=question.true
               :img=question.img
               :show="question.show"
+              :key="question.id"
               @wrong="pushAnswer"
               @next="next"></question>
+    <done v-if="this.done"
+          :title="winnerTitle"
+          :text="winnerText"
+          :right="right"
+          :length="this.questions.length"></done>
   </div>
 </template>
 
 <script>
 import Hello from './components/Hello';
 import Question from './components/Question';
+import Done from './components/Done';
 
 export default {
   name: 'app',
@@ -23,6 +31,11 @@ export default {
     return {
       counter: 0,
       wrong: 0,
+      right: 0,
+      done: false,
+      started: false,
+      winnerTitle: null,
+      winnerText: null,
       questions: [
         {
           answerOne: 'Сочи, Россия',
@@ -45,21 +58,100 @@ export default {
           img: '3',
           show: false,
         },
+        {
+          answerOne: 'Барселона, Испания',
+          answerTwo: 'Аляска, Россия',
+          true: 2,
+          img: '3',
+          show: false,
+        },
+        {
+          answerOne: 'Барселона, Испания',
+          answerTwo: 'Аляска, Россия',
+          true: 2,
+          img: '3',
+          show: false,
+        },
+        {
+          answerOne: 'Барселона, Испания',
+          answerTwo: 'Аляска, Россия',
+          true: 2,
+          img: '3',
+          show: false,
+        },
+        {
+          answerOne: 'Барселона, Испания',
+          answerTwo: 'Аляска, Россия',
+          true: 2,
+          img: '3',
+          show: false,
+        },
+        {
+          answerOne: 'Барселона, Испания',
+          answerTwo: 'Аляска, Россия',
+          true: 2,
+          img: '3',
+          show: false,
+        },
+        {
+          answerOne: 'Барселона, Испания',
+          answerTwo: 'Аляска, Россия',
+          true: 2,
+          img: '3',
+          show: false,
+        },
+        {
+          answerOne: 'Барселона, Испания',
+          answerTwo: 'Аляска, Россия',
+          true: 2,
+          img: '3',
+          show: false,
+        },
       ],
     };
   },
   components: {
     Hello,
     Question,
+    Done,
   },
   methods: {
+    setWinnerText() {
+      if (this.right >= 0 && this.right <= 2) {
+        this.winnerTitle = 'Любитель конкурных карт';
+        this.winnerText = 'Ох, видимо, о разнообразии природы России вы знаете только по контурным картам. Не отчаивайтесь, мы знаем как вам помочь! Для того, чтобы узнать о красоте России, нужно всего лишь захотеть этого и прийти на выставку «100 заповедных лет» Игоря Шпиленика __ _____ 2017.';
+      }
+      if (this.right >= 3 && this.right <= 4) {
+        this.winnerTitle = 'На грани фола';
+        this.winnerText = 'Здорово, что первая ассоциация с Байкалом для вас не лимонад, но вот Курильские острова от Канарских вы едва сможете отличить. Мы никому об этом не расскажем, даже выход подскажем, как знания быстро прокачать. Приходите на выставку «100 заповедных лет» Игоря Шпиленика __ ____ 2017, открывайте для себя заповедную природу России во всей ее захватывающей красоте.';
+      }
+      if (this.right >= 5 && this.right <= 6) {
+        this.winnerTitle = 'Профессионал инста-серфинга!';
+        this.winnerText = 'Понимаем, у вас очень много дел! Так много, что разнообразие природы России вы видите только тогда, когда пролистываете фотографии друзей в социальных сетях. Сейчас самое время исправиться и узнать, что в России есть что-то кроме Сочи, где жаркие ночи, Питера и «нашего» Крыма. Приходите на выставку «100 заповедных лет» Игоря Шпиленика __ _____ 2017, открывайте для себя заповедную природу России во всей ее поражающей красоте.';
+      }
+      if (this.right >= 7 && this.right <= 8) {
+        this.winnerTitle = 'Зам главного Географа всея Руси';
+        this.winnerText = 'Отличный результат, можно смело хвастаться друзьям! Вы отлично чувствуете границы, но все же, для вас есть что-то неизвестное. И это хорошо, значит природе России есть, чем вас удивить! Для прокачки знаний мы приглашаем вас на выставку «100 заповедных лет» Игоря Шпиленика __ _____ 2017, где мы покажем то, от чего у вас, наверняка, захватит дух!';
+      }
+      if (this.right >= 9 && this.right <= 10) {
+        this.winnerTitle = 'Поздравляем, вы Географ всея Руси';
+        this.winnerText = 'Вот это да! Вы настолько круто знаете заповедную природу России, что сам ведущий программы «Вокруг Света» вам завидует. Не смотря на все ваши королевские знания, вам будет любопытно заглянуть на выставку «100 заповедных лет» Игоря Шпиленика. Наверняка вы найдете что-то интересное и неизведанное для себя.';
+      }
+    },
     pushAnswer() {
       this.wrong = this.wrong + 1;
     },
     next() {
+      this.started = true;
       if (this.counter >= this.questions.length) {
-        console.log(this.questions.length - this.wrong, '/', this.questions.length);
+        this.done = true;
+        this.questions[this.counter - 1].show = false;
+        this.right = this.questions.length - this.wrong;
+        this.setWinnerText();
         return;
+      }
+      if (this.counter !== 0) {
+        this.questions[this.counter - 1].show = false;
       }
       this.questions[this.counter].show = true;
       this.counter = this.counter + 1;
@@ -92,5 +184,11 @@ html
 
 .layout
   width 960px
+  height 100%
   margin auto
+  display flex 
+  justify-content center
+  align-items center
+  flex-direction column
+  text-align center
 </style>
---
+++ src/components/Done.vue
@@ -0,0 +1,66 @@
+<template lang="pug">
+  .done
+    .layout
+      .done__content
+        h2 {{ this.title }}
+        p {{ this.text }}
+        span {{ this.right }} / {{ this.length }}
+        .social_buttons
+          button(@click="share.fb()") fb
+          button(@click="share.vk()") vk    
+          button(@click="share.ok()") ok      
+</template>
+
+<script>
+export default {
+  name: 'done',
+  props: [
+    'done',
+    'right',
+    'length',
+    'title',
+    'text',
+  ],
+  methods: {
+    share: {
+      fb() {
+        console.log('share fb');
+      },
+      vk() {
+        console.log('share vk');
+      },
+      ok() {
+        console.log('share ok');
+      },
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus">
+  .done 
+    position absolute
+    top 0
+    left 0
+    right 0
+    bottom 0
+    background-image url('./../assets/2.png')
+    background-size cover
+    background-position center
+    &__content 
+      position relative
+    img
+      position absolute
+      top 0
+      left 0
+      width 100%
+      height 100%
+      object-fit cover
+    h2
+    p 
+      color #fff
+    span 
+      color #fff
+      font-size 24px
+</style>
